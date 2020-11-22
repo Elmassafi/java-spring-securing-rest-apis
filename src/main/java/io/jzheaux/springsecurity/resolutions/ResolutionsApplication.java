b61ac49c21a16c3767667b9a49d5c6ac3435e3f5
Anas EL MASSAFI <anas.elmassafi@gmail.com>
2020-11-22 15:52:38 +0100

    Replace Defaults With an In-memory User Datasource

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
@@ -2,13 +2,30 @@ package io.jzheaux.springsecurity.resolutions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+/*
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+excluding SecurityAutoConfiguration.class will disable the autoConfiguration of spring security
+to apply the Spring Boot Security Starter's auto-configuration to the project remove the exclusion
+ */
+@SpringBootApplication()
 public class ResolutionsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ResolutionsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ResolutionsApplication.class, args);
+    }
 
+    @Bean
+    UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user")
+                        .password("{bcrypt}$2a$10$MywQEqdZFNIYnx.Ro/VQ0ulanQAl34B5xVjK2I/SDZNVGS5tHQ08W")
+                        .authorities("resolution:read")
+                        .build()
+        );
+    }
 }
