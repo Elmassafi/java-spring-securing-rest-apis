622f17b12cb10e2b4e467a07d6cc705184c91942
Anas EL MASSAFI <anas.elmassafi@gmail.com>
2020-11-22 16:10:05 +0100

    Replace In-memory User Datasource with Jdbc User Datasource and hardcoded the resolution:read authority into every user

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
@@ -3,9 +3,13 @@ package io.jzheaux.springsecurity.resolutions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.sql.DataSource;
+import java.util.List;
 
 /*
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
@@ -20,12 +24,23 @@ public class ResolutionsApplication {
     }
 
     @Bean
-    UserDetailsService userDetailsService() {
-        return new InMemoryUserDetailsManager(
+    UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource) {
+            /*
+            We haven't taken any steps yet to represent authority.
+            Thus, the above declaration hardcodes the resolution:read authority into every user.
+             */
+            @Override
+            protected List<GrantedAuthority> loadUserAuthorities(String username) {
+                return AuthorityUtils.createAuthorityList("resolution:read");
+            }
+        };
+    }
+}
+/*
+return new InMemoryUserDetailsManager(
                 User.withUsername("user")
                         .password("{bcrypt}$2a$10$MywQEqdZFNIYnx.Ro/VQ0ulanQAl34B5xVjK2I/SDZNVGS5tHQ08W")
                         .authorities("resolution:read")
                         .build()
-        );
-    }
-}
+ */
